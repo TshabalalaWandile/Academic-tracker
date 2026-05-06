9c2e9a93b4ab94cedb5c2b6a5cac7a4c28e5a56e
Wandile <wandiletshabalala83@gmail.com>
2026-05-06 13:08:18 +0200

    Further enhancements of the design

--- Academic tracker/App.xaml.cs
+++ Academic tracker/App.xaml.cs
@@ -12,5 +12,15 @@ namespace Academic_tracker
             var loginPage = new LoginPage(db);
             MainPage = new NavigationPage(loginPage);
         }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 750;
+
+            return window;
+        }
     }
 }
